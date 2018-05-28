f24ab250f1e0cdc22bb7dcba8efdbc1bda0936b4
Andrea Witt <andreawitt14@hotmail.com>
2018-05-28 18:04:47 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,11 @@
 function forLoop(array){
   for(let i = 0; i < 25; i++){
     if (i == 1)
-    array[i] = "I am ${i} strange loop"
+    array[i] = "I am ${i} strange loop";
     else
-    array[i] = "I am ${i} strange loops"
+    array[i] = "I am ${i} strange loops";
   }
-    return array 
+    return array; 
 }
 
 function whileLoop(number){
